b15247351be0fad15651a6b7cb8e40a9066d692e
Danifalha11 <mariadelourdessilveira142@gmail.com>
2026-09-17 15:06:48 -0300

    Add MySQL connection class for database access

--- mysql.cs
+++ mysql.cs
@@ -1,1 +1,20 @@
-teste
+using MySql.Data.MySqlClient;
+
+public class Conexao
+{
+    private string conexao =
+        "Server=localhost;" +
+        "Database=cinema;" +
+        "Uid=root;" +
+        "Pwd=Senac2026;";
+
+    public MySqlConnection Conectar()
+    {
+        MySqlConnection conn =
+            new MySqlConnection(conexao);
+
+        conn.Open();
+
+        return conn;
+    }
+}
